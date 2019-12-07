9ebf56fa6207336dc0f1df2782aba47936ab395b
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-06 22:02:20 -0800

    final touch

--- gen.py
+++ gen.py
@@ -27,7 +27,10 @@ def toParagraph(vmess):
 
 def process(item):
     if item.startswith(vmscheme):
-        vmess_results.add(toParagraph(parseVmess(item)))
+        try:
+            vmess_results.add(toParagraph(parseVmess(item)))
+        except Exception as e:
+            return
     if item.startswith(telegram_proxy):
         telegram_results.add(item)
 
@@ -42,7 +45,7 @@ for subdir, dirs, files in os.walk('data'):
 S1 = '请在各个平台（mac/windows/linus, ios/andriod）安装V2Ray之后输入以下配置，即可登录谷歌，脸书，油管等网站。\n'
 S2 = '以下是一些Telegram上的proxy，也欢迎用：'
 # This is all ok to commit, not commited by mistake.
-S3 = '''如果您不想安装app 用电脑端想翻墙的话，可用网页型翻墙，站点如下：
+S3 = '''\n如果您不想安装app 用电脑端想翻墙的话，可用网页型翻墙，站点如下：
 https://misty-disk-50cc.networkfreedom.workers.dev/
 https://green-credit-9e7f.networkfreedom.workers.dev/
 https://lucky-mountain-6e91.networkfreedom.workers.dev/
@@ -52,9 +55,12 @@ Telegram群：https://t.me/dushufenxiang_chat
 Facebook读书群：https://www.facebook.com/groups/reading.sharing/
 电子书网站：https://readmoo.com （我的账号是bajie90@gmail.com，密码是dushufenxiang，欢迎大家共用这个账号读书。）
 
-节点由 https://t.me/cnhumanright99, https://t.me/jianjiaobuluo 等提供'''
+节点由 https://t.me/cnhumanright99, https://t.me/jianjiaobuluo 等提供。'''
 result = [S1]
-result += random.sample(vmess_results, min(3, len(vmess_results)))
+count = 0
+for item in random.sample(vmess_results, min(3, len(vmess_results))):
+    count += 1
+    result.append('设置' + str(count) + ':\n' + item + '\n')
 result.append(S2)
 result += random.sample(telegram_results, min(3, len(telegram_results)))
 result.append(S3)
